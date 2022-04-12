0ce0bbfa2697c9c2347acab9673712310a65b47e
ElsayedMosad <87620538+ElsayedMosad@users.noreply.github.com>
2022-04-12 06:39:17 +0200

    Play Nav Menu

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+
+
+// Genral functions
+function removeClass(ArrayOfElements, classTargetToRemove) {
+  ArrayOfElements.forEach((element) => {
+    if (element.classList.contains(classTargetToRemove)) {
+      element.classList.remove(classTargetToRemove);
+    }
+  });
+}
+const leftSide = document.querySelector(".left-side");
+const navLinks = document.querySelectorAll(".nav .nav-link");
+// console.log(leftSide);
+
+// add active link to a clicked
+navLinks.forEach((e, index) => {
+  e.addEventListener("click", function () {
+    if (!e.classList.contains("active-link")) {
+      removeClass(navLinks, "active-link");
+      e.classList.add("active-link");
+      if (index === 0 || index === navLinks.length - 1) {
+        if (leftSide.classList.contains('play-left')) {
+          leftSide.classList.remove('play-left')
+        }
+      }
+      else if (!leftSide.classList.contains('play-left')) {
+        leftSide.classList.add('play-left')
+      }
+    }
+  });
+});
